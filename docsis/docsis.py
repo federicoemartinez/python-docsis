4927e3c9c62aec15598a0ca1eae5137b810f0b06
Federico <federicoemartinez@gmail.com>
2020-02-27 09:52:24 -0300

    updated to work with the latest version of libdocsis 0.2 that requires initialization and is thread-safe

--- docsis/docsis.py
+++ docsis/docsis.py
@@ -1,21 +1,28 @@
 from ctypes import *;
 from ctypes.util import find_library
+from threading import Lock
 
 libdocsis_path = None
 libdocsis = None
 char_pointer = POINTER(c_ubyte)
 char_pointer_pointer = POINTER(POINTER(c_ubyte))
 libc = CDLL(find_library('c'))
+lib_lock = Lock()
 
 def set_libdocsis_path(path):
 	global libdocsis_path
 	libdocsis_path = path
 
 def load_libdocsis():
+	lib_lock.acquire()
+	if libdocsis is not None:
+		return
 	global libdocsis, libdocsis_path
 	if libdocsis_path is None:
 		libdocsis_path = './libdocsis.so'
 	libdocsis = cdll.LoadLibrary(libdocsis_path)
+	libdocsis.initialize()
+	lib_lock.release()
 
 def encode_content(content, key, encode_docsis=True, hash= 0):
 	#hash = 2 means eu
@@ -23,22 +30,28 @@ def encode_content(content, key, encode_docsis=True, hash= 0):
 	global libdocsis, libc
 	if libdocsis is None:
 		raise Exception('You have to load libdocsis first!')
+
 	p = char_pointer()
 	p1 = char_pointer_pointer(p)
+
 	output_size = libdocsis.encode_file(content, len(content),p1, key,len(key),1 if encode_docsis else 0,hash) 
 	if output_size == 0: raise Exception('Unable to encode file')
 	ret = ''.join(chr(p1.contents[i]) for i in range(output_size))
 	libc.free(p)
+	
+	if len(ret) == 0: raise Exception('Unable to encode file')
 	return ret
 
 
 if __name__ == '__main__':
 	load_libdocsis()
-	ifile = file('docsis.txt', 'rb')
+	ifile = open('docsis.txt', 'rb')
 	content = ifile.read()
-	ret = encode_content(content, 'a key', encode_docsis=True, hash= 1)
+	print(content)
+	ret = encode_content(content, 'a key', encode_docsis=True, hash= 0)
 	out = open('docsis.bin', 'wb')
-	out.write(ret)
+	#print(ret)
+	out.write(ret.encode('utf8'))
 	out.close()
 
 
